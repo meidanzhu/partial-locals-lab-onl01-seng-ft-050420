c5fba0763b7eeacd5989afd1576a2fc7544bae35
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-07-23 01:16:51 +0000

    Automatically backed up by Learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,13 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  def self.search(search)
+    if search
+      @students = Student.where("name LIKE ?", "%#{search}%")
+    else
+      @students = Student.all
+    end
+  end
+  
 end
